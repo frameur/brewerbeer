f8e2d8a4687cea89cb4ac0e4ea06bd96b3b8d3c9
frameur <francoisramos6121@gmail.com>
2020-06-21 16:09:14 +0200

    mise en place base view auth users

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-const express = require('express')
-const ejs = require('ejs')
-const router = express.Router()
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+const port = 1998;
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Route
+const { getHomePage} = require('./routes/index');
+
+//api
+app.get('/', getHomePage)
+
+//Template
+app.set('view engine', 'ejs');
+
+// Express Static
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(port, () => {
+    console.log(`lancer sur le port ${port}`);
+    
+});
