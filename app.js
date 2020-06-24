f008dfad866705f01be44d830a97fda2d18f6cab
frameur <francoisramos6121@gmail.com>
2020-06-24 11:38:13 +0200

    dossier public et install fileupload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const ejs = require('ejs');
+const fileUpload = require('express-fileupload');
 const path = require('path');
 const session = require('express-session');
 const mysql = require('mysql');
@@ -11,6 +11,7 @@ app.set('view engine', 'ejs');
 
 // Express Static
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
 
 // Middleware
 app.use(express.json());
@@ -40,12 +41,30 @@ db.connect((err) => {
 });
 global.db = db;
 
+// Controller
+// const homeRoutes = require('./routes/home');
+//admin
+// const adminRoutes = require('./routes/admin');
+// const authRoutes = require('./routes/auth');
+
+// Admin
+// app.use('/admin', auth,  adminRoutes);
+
+// Authentification
+// app.use('/auth',  authRoutes);
+
 // Route
 const { getHomePage} = require('./routes/home');
 
 //api
 app.get('/', getHomePage)
 
+app.get('*', function(req, res, next){
+  res.status(404);
+  res.render('404.ejs', {
+      title: "Cette page n'existe pas.",
+  });
+});
 
 
 
