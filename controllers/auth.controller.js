7d7396ad4f64e2fe8a661c9f6adf2e34da7c22cb
frameur <60637865+frameur@users.noreply.github.com>
2020-06-29 14:49:55 +0200

    allegement code authcontroller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -16,8 +16,15 @@ exports.registerPage = (req, res) => {
 
 // Post insription
 exports.register = (req, res) => {
-  
-  let {firstname, lastname, email, age, password, passwordConfirm} = req.body;
+
+  let {
+    firstname,
+    lastname,
+    email,
+    age,
+    password,
+    passwordConfirm
+  } = req.body;
 
   let emailQuery = "SELECT * FROM `users` WHERE email = '" + email + "'";
 
@@ -28,15 +35,15 @@ exports.register = (req, res) => {
     if (result.length > 0) {
       message = "Le compte existe déjà";
       res.redirect("auth/register", {
-      message,
-       title: "Ajouter un utilisateur",
-      }); 
-    
-    } else if(password !== passwordConfirm){
-      return res.render('register', {
-          message: 'Password different'
+        message,
+        title: "Ajouter un utilisateur",
       });
-  } else {
+
+    } else if (password !== passwordConfirm) {
+      return res.render('register', {
+        message: 'Password different'
+      });
+    } else {
 
       bcrypt.hash(password, 10, function (err, hash) {
 
@@ -65,55 +72,55 @@ exports.register = (req, res) => {
   })
 }
 
+
 // Login
-
 exports.login = (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+	const email = req.body.email;
+	const password = req.body.password;
   
-  db.query('SELECT user_id FROM users WHERE email = ?', [email], (err, result) => {
-
-    if (err || result.length === 0) {
-      console.log("result :", result);
-
-      return res.status(401).json({
-        error: `Vous n'êtes pas inscrit`
-      });
-    } else {
-
-      bcrypt.compare(password, result[0].password, (err, success) => {
-        if (err) {
-          return res.status(401).json({
-            error: `Bcrypt Auth failed`
-          });
-        }
-        if (success) {
-
-          db.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, result[0].password], function (err, results) {
-
-            if (results.length) {
-              req.session.loggedin = true;
-              req.session.firstname = results[0].firstname;
-              req.session.userId = SpeechRecognitionResultList[0].id;
-
-
-              res.redirect('/');
-
-              console.log("req.session :", req.session)
-
-            } else {
-              res.send('Email ou mot de passe incorrect !');
-            }
-          });
-        } else {
-          res.send('Ajouter un email ou un mot de passe !');
-        }
-      })
-    }
-  })
-};
-
-
+	db.query('SELECT * FROM users WHERE email = ?', [email], (err, result) => {
+  
+	  if (err || result.length === 0) {
+		console.log("result :", result);
+  
+		return res.status(401).json({
+		  error: `Vous n'êtes pas inscrit`
+		});
+	  } else {
+  
+		bcrypt.compare(password, result[0].password, (err, success) => {
+		  if (err) {
+			return res.status(401).json({
+			  error: `Bcrypt Auth failed`
+			});
+		  }
+		  if (success) {
+  
+			db.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, result[0].password], function (err, results) {
+  
+			  if (results.length) {
+				req.session.loggedin = true;
+				req.session.firstname = results[0].firstname;
+				req.session.user_id = SpeechRecognitionResultList[0].id;
+  
+  
+				res.redirect('/');
+  
+				console.log("req.session :", req.session)
+  
+			  } else {
+				res.send('Email ou mot de passe incorrect !');
+			  }
+			});
+		  } else {
+			res.send('Ajouter un email ou un mot de passe !');
+		  }
+		})
+	  }
+  
+	})
+  
+  }
 
 exports.logout = (req, res) => {
   req.session.destroy((err) => {
