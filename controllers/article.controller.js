146a232e8c4e92ce04a1376d8ea8834df6004b75
frameur <60637865+frameur@users.noreply.github.com>
2020-07-10 11:41:50 +0200

    async await controller articles

--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -1,27 +1,18 @@
-exports.addArticlePage = (req, res) => {
-    let query = [
-        "SELECT `actu_id`, `actuTitle`, `actuContent`, `Date`, `created_at` FROM `actubeer` ORDER BY actu_id ASC LIMIT 10",
-        "SELECT COUNT(*) AS count FROM actubeer"
-      ]
-    
-    db.query(query.join(';'), (err, result) => {
-       
-        if (err) {
-            res.redirect('/admin/dashboard');
-        }
-        console.log("result :", result[1][0].count)
-      
+exports.addArticlePage = async (req, res) => {
+    const listarticles = await queryAsync("SELECT `actu_id`, `actuTitle`, `actuContent`, `Date`, `created_at` FROM `actubeer` ORDER BY actu_id ASC LIMIT 10")
         
+    const totalActubeers = await queryAsync("SELECT COUNT(*) AS count FROM actubeer")    
+      
+ 
         res.render('admin/articles', {
           
-            
            title: "liste des articles",
-           actubeer: result,
-           totalActubeers: result[1][0].count
+           actubeer: listarticles,
+           totalActubeers: totalActubeers[0].count
             
   
         });
-    });
+    
      }
   
 
