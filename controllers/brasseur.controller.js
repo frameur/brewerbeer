a5e170658eed89b7cda38f2b16c40235dae52b17
frameur <60637865+frameur@users.noreply.github.com>
2020-07-12 17:31:52 +0200

    affichage fiche individuel brasseur

--- controllers/brasseur.controller.js
+++ controllers/brasseur.controller.js
@@ -1,11 +1,11 @@
-
+const fs = require('fs');
 //affiche les brasseurs
 exports.addBrasseurPage = async (req, res) => {
     
     const listbrewer = await queryAsync("SELECT `brewer_id`, `nameBrass`, `address`, `nameCp`, `nameTown`, `nameWeb`, `nameFacebook`, `email`, `phone`, `logo`, `content`, `listBeer`, `created_at` FROM `brewersfrench` ORDER BY brewer_id ASC LIMIT 25")
     const totalBrewers = await queryAsync ("SELECT COUNT(*) AS count FROM brewersfrench")
-
-      
+    
+   
         res.render('admin/brasseurs', {
           
             
@@ -19,9 +19,9 @@ exports.addBrasseurPage = async (req, res) => {
 //affiche un brasseur
 exports.editBrasseurPage = async(req, res) =>{
 
-    let brewerid = req.params.brewer_id;
-    const fichebrewer = await queryAsync ("SELECT * FROM `brewersfrench` WHERE brewer_id = '" + brewerid + "' ")
-
+    let brewerid = req.params.id;
+    const fichebrewer = await queryAsync ("SELECT `brewer_id`, `nameBrass`, `address`, `nameCp`, `nameTown`, `nameWeb`, `nameFacebook`, `email`, `phone`, `logo`, `content`, `listBeer`, `created_at` FROM `brewersfrench` WHERE brewer_id = '" + brewerid + "' ")
+    console.log("result :", fichebrewer);
         res.render('admin/brasseurone', {
         title: "fiche brasseur",
         brewerone:fichebrewer[0]   
