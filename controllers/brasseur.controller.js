87de1cd3d1a0bf9ac79f84ed3408392ceab99ee5
frameur <60637865+frameur@users.noreply.github.com>
2020-07-13 20:56:01 +0200

    gestion err try et catch brasseurcontroller

--- controllers/brasseur.controller.js
+++ controllers/brasseur.controller.js
@@ -2,105 +2,100 @@ const fs = require('fs');
 //affiche les brasseurs
 exports.addBrasseurPage = async (req, res) => {
 
-    const listbrewer = await queryAsync("SELECT `brewer_id`, `nameBrass`, `address`, `nameCp`, `nameTown`, `nameWeb`, `nameFacebook`, `email`, `phone`, `logo`, `content`, `listBeer`, `created_at` FROM `brewersfrench` ORDER BY brewer_id ASC LIMIT 25")
-    const totalBrewers = await queryAsync("SELECT COUNT(*) AS count FROM brewersfrench")
+    try {
 
-    console.log("result :", listbrewer);
-    res.render('admin/brasseurs', {
+        const listbrewer = await queryAsync("SELECT `brewer_id`, `nameBrass`, `address`, `nameCp`, `nameTown`, `nameWeb`, `nameFacebook`, `email`, `phone`, `logo`, `content`, `listBeer`, `created_at` FROM `brewersfrench` ORDER BY brewer_id ASC LIMIT 25")
+        const totalBrewers = await queryAsync("SELECT COUNT(*) AS count FROM brewersfrench")
 
+        res.render('admin/brasseurs', {
 
-        title: "liste des brasseurs",
-        brewersfrench: listbrewer,
-        totalBrewers: totalBrewers[0].count
+            title: "liste des brasseurs",
+            brewersfrench: listbrewer,
+            totalBrewers: totalBrewers[0].count
 
-    })
+        })
+    
+    } catch (err) {
+        console.log(err.message);
+    }
+
 
 }
 //Ajouter un brasseur
 exports.addBrasseur = async (req, res) => {
 
-    let {
-        nameBrass,
-        address,
-        nameCp,
-        nameTown,
-        nameWeb,
-        nameFacebook,
-        email,
-        phone,
-        logo,
-        content,
-        listBeer,
-        created_at
-    } = req.body;
+    try {
+
+        let {nameBrass, address, nameCp, nameTown, nameWeb, nameFacebook, email, phone, logo, content, listBeer, created_at} = req.body;
+
+        const brewerajout = await queryAsync("INSERT INTO `brewersfrench`(`brewer_id`, `nameBrass`, `address`, `nameCp`, `nameTown`, `nameWeb`, `nameFacebook`, `email`, `phone`, `logo`, `content`, `listBeer`, `created_at`) VALUES ('" + brewer_id + "', '" + nameBrass + "', '" + address + "', '" + nameCp + "', '" + nameTown + "', '" + nameWeb + "', '" + nameFacebook + +"', '" + email + "', '" + phone + "', '" + logo + "', '" + content + "', '" + listBeer + "', '" + created_at + "')")
+        // console.log("result :", brewerajout);
+        res.render('admin/brasseuradd', {
+            message,
+            title: "Ajouter un brasseur",
+            breweradd: brewerajout
+
+        });
     
- 
-    const brewerajout = await queryAsync ("INSERT INTO `brewersfrench`(`brewer_id`, `nameBrass`, `address`, `nameCp`, `nameTown`, `nameWeb`, `nameFacebook`, `email`, `phone`, `logo`, `content`, `listBeer`, `created_at`) VALUES ('" + brewer_id + "', '" + nameBrass + "', '" + address + "', '" + nameCp + "', '" + nameTown + "', '" + nameWeb + "', '" + nameFacebook + +"', '" + email + "', '" + phone + "', '" + logo + "', '" + content + "', '" + listBeer + "', '" + created_at + "')")
-
-    // message = "Fichier image invalide";
-    res.render('admin/brasseuradd', {
-        message,
-        title: "Ajouter un brasseur",
-        breweradd:brewerajout
-
-    });
+    } catch (err) {
+        console.log(err.message);
+    }
 }
 
 //affiche un brasseur
 exports.editBrasseurPage = async (req, res) => {
 
-    let brewerid = req.params.id;
+    try {
 
-    const fichebrewer = await queryAsync("SELECT `brewer_id`, `nameBrass`, `address`, `nameCp`, `nameTown`, `nameWeb`, `nameFacebook`, `email`, `phone`, `logo`, `content`, `listBeer`, `created_at` FROM `brewersfrench` WHERE brewer_id = '" + brewerid + "' ")
+        let brewerid = req.params.id;
 
-    res.render('admin/brasseurone', {
-        title: "fiche brasseur",
-        brewerone: fichebrewer[0]
+        const fichebrewer = await queryAsync("SELECT `brewer_id`, `nameBrass`, `address`, `nameCp`, `nameTown`, `nameWeb`, `nameFacebook`, `email`, `phone`, `logo`, `content`, `listBeer`, `created_at` FROM `brewersfrench` WHERE brewer_id = '" + brewerid + "' ")
 
-    })
+        res.render('admin/brasseurone', {
+            title: "fiche brasseur",
+            brewerone: fichebrewer[0]
+        })
+    } catch (err) {
+        console.log(err.message);
+    }
 
 }
 //modifier une fiche brasseur
 exports.editBrasseur = async (req, res) => {
-    let brewerId = req.params.id;
 
-    let {
-        nameBrass,
-        address,
-        nameCp,
-        nameTown,
-        nameWeb,
-        nameFacebook,
-        email,
-        phone,
-        logo,
-        content,
-        listBeer,
-        created_at
-    } = req.body;
+    try {
 
+        let brewerId = req.params.id;
 
-    const brewermodif = await queryAsync("UPDATE `players` SET `nameBrass` = '" + nameBrass + "', `address` = '" + address + "', `nameCp` = '" + nameCp + "', `nameTown` = '" + nameTown + "' , `nameWeb` = '" + nameWeb + "' , `nameFacebook` = '" + nameFacebook + "' , `email` = '" + email + "' , `phone` = '" + phone + "' , `logo` = '" + logo + "' , `content` = '" + content + "' , `listBeer` = '" + listBeer + "' , `created_at` = '" + created_at + "' WHERE `brewersfrench`.`brewer_id` = '" + brewerId + "'");
-    
-        
+        let {nameBrass, address, nameCp, nameTown, nameWeb, nameFacebook, email, phone, logo, content, listBeer, created_at} = req.body;
+
+        const brewermodif = await queryAsync("UPDATE `players` SET `nameBrass` = '" + nameBrass + "', `address` = '" + address + "', `nameCp` = '" + nameCp + "', `nameTown` = '" + nameTown + "' , `nameWeb` = '" + nameWeb + "' , `nameFacebook` = '" + nameFacebook + "' , `email` = '" + email + "' , `phone` = '" + phone + "' , `logo` = '" + logo + "' , `content` = '" + content + "' , `listBeer` = '" + listBeer + "' , `created_at` = '" + created_at + "' WHERE `brewersfrench`.`brewer_id` = '" + brewerId + "'");
+
         res.redirect('/brass/list', {
-            brewermodif:brewermodif
+            brewermodif: brewermodif
         });
 
-    
+    } catch (err) {
+        console.log(err.message);
+    }
+
 }
 //supprimer un fichier brasseur
 exports.deleteBrasseur = async (req, res) => {
-    let brewerId = req.params.id;
-    let brewerdelete = await queryAsync ('DELETE FROM brewerfrench WHERE brewer_id = "' + brewerId + '"')
-    
- 
+
+    try {
+        let brewerId = req.params.id;
+        let brewerdelete = await queryAsync('DELETE FROM brewerfrench WHERE brewer_id = "' + brewerId + '"')
+
         res.redirect('/brass/list', {
-                    
+
             message,
             title: 'fiche brasseur supprimer',
-            brewerdel:brewerdelete
+            brewerdel: brewerdelete
         });
-            
-    
+
+    } catch (err) {
+        console.log(err.message);
+    }
+
 }
