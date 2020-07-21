0487883813b7675b7133c7cb66fb1c5e3e8a786d
frameur <francoisramos6121@gmail.com>
2020-07-22 00:06:37 +0200

    code modifier dans querysql

--- query.sql
+++ query.sql
@@ -22,3 +22,71 @@ SELECT `departement_code`, `departement_nom_uppercase` FROM `departement` WHERE 
 SELECT  `ville_departement`, `ville_nom`, `ville_nom_simple`, `ville_nom_reel`, `ville_code_postal`,  `ville_longitude_deg`, `ville_latitude_deg`  FROM `villes_france_free` WHERE 1
 
 SELECT DATE_FORMAT(A.created_at, "% m /% d /% Y% T") AS created_at
+
+ <!-- <script>
+         // https://esri.github.io/esri-leaflet/examples/geocoding-control.html
+
+         const coordsParis = {
+            lat: 48.866667,
+            lng: 2.333333
+         };
+         let coordsFromBrowser = {
+            lat: coordsParis.lat,
+            lng: coordsParis.lng
+         };
+
+         navigator.geolocation.getCurrentPosition(function (position) {
+            console.log(
+               "position",
+               position.coords.latitude,
+               position.coords.longitude
+            );
+
+            coordsFromBrowser.lat = position.coords.latitude;
+            coordsFromBrowser.lng = position.coords.longitude;
+
+            map.setView([coordsFromBrowser.lat, coordsFromBrowser.lng], 15);
+         });
+
+         const map = L.map("map").setView(
+            [coordsFromBrowser.lat, coordsFromBrowser.lng],
+            7
+         );
+
+         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
+         }).addTo(map);
+
+         const searchControl = L.esri.Geocoding.geosearch().addTo(map);
+
+         const results = L.layerGroup().addTo(map);
+
+         let markers = [];
+
+         searchControl.on("results", function (data) {
+            markers = [];
+            console.log("data", data);
+            results.clearLayers();
+            // several results as several towns with same name (like)
+            for (var i = data.results.length - 1; i >= 0; i--) {
+               const result = data.results[i];
+               const marker = L.marker(result.latlng);
+               markers = [...markers, L.marker(marker)];
+               results.addLayer(marker);
+               marker.on("click", addRadius);
+               console.log("markers", markers);
+            }
+         });
+
+         function addRadius(marker, radius = 1000) {
+            console.log("marker clicked", marker);
+            const circle = L.circle([marker.latlng.lat, marker.latlng.lng], {
+               radius,
+            });
+            console.log("circle", circle);
+            circle.addTo(map);
+            setTimeout(() => {
+               map.setZoom(15);
+            }, 1000);
+         }
+      </script> -->
