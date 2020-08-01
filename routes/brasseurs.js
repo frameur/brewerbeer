e5eed8c091afced6505e6e6d2331ec1cb93d0b04
frameur <60637865+frameur@users.noreply.github.com>
2020-08-01 19:55:09 +0200

    integration carte page brasseurs

--- routes/brasseurs.js
+++ routes/brasseurs.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const brasseursController = require('../controllers/brasseurs.controller')
 
 //affiche liste brasseurs et brasseur individuel
-router.get('/brasseurs', brasseursController.getBrasseur)
 
 router.get('/brasseurs-api', brasseursController.getBrasseurAPI)
+router.get('/brasseurs', brasseursController.getBrasseur)
 
 
 
