b634a59f7c5d775dd599df8d57538bfa29a74992
frameur <60637865+frameur@users.noreply.github.com>
2020-08-04 23:28:38 +0200

    new carte brasseurs

--- public/map.js
+++ public/map.js
@@ -1,37 +1,82 @@
-  // On initialise la latitude et la longitude de Paris (centre de la carte)
-      var lat = 48.852969;
-      var lon = 2.349903;
-      var macarte = null;
-           var markerClusters; // Servira à stocker les groupes de marqueurs
-           // Nous initialisons une liste de marqueurs
-       
-  
-           var brewers = {
-               "Paris": { "lat": 48.852969, "lon": 2.349903 },
-               "Brest": { "lat": 48.383, "lon": -4.500 },
-               "Quimper": { "lat": 48.000, "lon": -4.100 },
-               "Bayonne": { "lat": 43.500, "lon": -1.467 }
-           };
-      // Fonction d'initialisation de la carte
-     // Fonction d'initialisation de la carte
-  function initMap() {
-  // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
-  macarte = L.map('map').setView([lat, lon], 6);
-  // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
-  L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-     // Il est toujours bien de laisser le lien vers la source des données
-     attribution: 'données © OpenStreetMap/ODbL - rendu OSM France',
-     minZoom: 1,
-     maxZoom: 20
-  }).addTo(macarte);
-  // Nous parcourons la liste des villes
-  for (brewer in brewers) {
-  var marker = L.marker([brewers[brewer].lat, brewers[brewer].lon]).addTo(macarte);
-  // Nous ajoutons la popup. A noter que son contenu (ici la variable ville) peut être du HTML
-  marker.bindPopup(brewer);
-  }               	
-  }
-      window.onload = function(){
-     // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
-     initMap(); 
-      };
+
+            var map;
+            /* initialisation de la fonction initmap */
+            function initmap() {
+                // paramÃ©trage de la carte
+                map = new L.Map('map');
+                // crÃ©ation des "tiles" avec open street map
+                var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+                var osmAttrib='Map data de OpenStreetMap';
+                var osm = new L.TileLayer(osmUrl, {minZoom: 2, maxZoom: 10, attribution: osmAttrib});           
+                // on centre sur la France
+                map.setView(new L.LatLng(46.85, 2.3518),6);
+                map.addLayer(osm);
+            }
+            /* on va procÃ©der Ã  l'initialisation de la carte */
+            initmap();
+    
+            /* Creation d'un tableau qui va contenir nos donnes */
+            /*
+             * Pour chaque elt du tableay on a les coordonnÃ©es gÃ©ographiques
+             * une valeur ainsi que le nom de la rÃ©gion franÃ§aise
+             */
+            var departement = [
+                [48.58476, 7.750576,10,  'Alsace'],
+                [44.837912, -0.579541,7,  'Aquitaine'],
+                [45.783088, 3.082352,20,  'Auvergne'],
+                [47.32167, 5.04139,30,  'Bourgogne'],
+                [48.114722, -1.679444,30,  'Bretagne'],
+                [47.9025, 1.909,21,  'Centre'],
+                [48.9575, 4.365,32,  'Champagne-Ardenne'],
+                [41.9266, 8.73694,8,  'Corse'],
+                [47.24306, 6.02194,12,  'Franche-ComtÃ©'],
+                [16, -61.73334,13,  'Guadeloupe'],
+                [4.93461, -52.33033,4,  'Guyane'],
+                [48.856578, 2.351828,25,  'Ã�le-de-France'],
+                [43.611944, 3.877222,32,  'Languedoc-Roussillon'],
+                [45.85, 1.25,20,  'Limousin'],
+                [49.1203, 6.1778,35,  'Lorraine'],
+                [14.6, -61.08334,4,  'Martinique'],
+                [-12.77889, 45.23151,2,  'Mayotte'],
+                [43.604482, 1.443962,7,  'Midi-PyrÃ©nÃ©es'],
+                [50.637222, 3.063333,28,  'Nord-Pas-de-Calais'],
+                [49.183056, -0.369444,14,  'Basse-Normandie'],
+                [49.443889, 1.103333,18,  'Haute-Normandie'],
+                [47.21806, -1.55278,15,  'Pays de la Loire'],
+                [49.9, 2.3, 12, 'Picardie'],
+                [46.581945, 0.336112,21,  'Poitou-Charentes'],
+                [43.296346, 5.369889,32,  'Provence-Alpes-CÃ´te d\'Azur'],
+                [-20.8789, 55.4481,7,  'La RÃ©union'],
+                [45.759723, 4.842223,8,  'RhÃ´ne-Alpes'] 
+            ];
+            
+            /* On boucle sur le tableau pour y placer les points */
+            for (i = 0; i < departement.length; i++) {
+                
+                var nbAnnonces = departement[i][2];
+                var couleur ="green";
+                
+                if (nbAnnonces > 20000) {
+                    if (nbAnnonces > 50000) {
+                        couleur = "red";
+                    } else {
+                        couleur = "orange";
+                    }
+                }
+                /*
+                 * On va crÃ©er un cercle sur la carte pour chaque point
+                 */
+                var circleLocation = new L.LatLng(departement[i][0], departement[i][1]),
+                circleOptions = {
+                    color: couleur,
+                    fillColor: couleur,
+                    fillOpacity: 0.5
+                };
+        
+                var marker = new L.marker(circleLocation,(7000 + (departement[i][2]/4)), circleOptions);
+                // on rajoute un popup sur le cercle
+                marker.bindPopup(departement[i][3]+ " : " + departement[i][2]+" Brasseurs");
+                // on ajoute le cercle Ã  la carte
+                map.addLayer(marker);
+            }
+        
--- query.sql
+++ query.sql
@@ -184,3 +184,49 @@ INSERT INTO Ventes (ref, client_id, prix) VALUES (3, 1, 1500);
 
          getApi();
       </script>
+
+
+   <!-- Nous chargeons les fichiers CDN de Leaflet. Le CSS AVANT le JS -->
+   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==" crossorigin="" />
+   <link rel="stylesheet" type="text/css" href="https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.css" />
+   <link rel="stylesheet" type="text/css" href="https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.Default.css" />
+    <!-- Fichiers Javascript -->
+    <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
+    <script type='text/javascript' src='https://unpkg.com/leaflet.markercluster@1.3.0/dist/leaflet.markercluster.js'></script>
+        // On initialise la latitude et la longitude de Paris (centre de la carte)
+      var lat = 48.852969;
+      var lon = 2.349903;
+      var macarte = null;
+           var markerClusters; // Servira à stocker les groupes de marqueurs
+           // Nous initialisons une liste de marqueurs
+          
+  
+           var brewers = {
+               "Paris": { "lat": 48.852969, "lon": 2.349903 },
+               "Brest": { "lat": 48.383, "lon": -4.500 },
+               "Quimper": { "lat": 48.000, "lon": -4.100 },
+               "Bayonne": { "lat": 43.500, "lon": -1.467 }
+           };
+      // Fonction d'initialisation de la carte
+     // Fonction d'initialisation de la carte
+  function initMap() {
+  // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
+  macarte = L.map('map').setView([lat, lon], 6);
+  // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+     // Il est toujours bien de laisser le lien vers la source des données
+     attribution: 'données © OpenStreetMap/ODbL - rendu OSM France',
+     minZoom: 1,
+     maxZoom: 20
+  }).addTo(macarte);
+  // Nous parcourons la liste des villes
+  for (brewer in brewers) {
+  var marker = L.marker([brewers[brewer].lat, brewers[brewer].lon]).addTo(macarte);
+  // Nous ajoutons la popup. A noter que son contenu (ici la variable ville) peut être du HTML
+  marker.bindPopup(brewer);
+  }               	
+  }
+      window.onload = function(){
+     // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
+     initMap(); 
+      };
